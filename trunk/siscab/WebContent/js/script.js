c4a4611d86b792c1469e7429c66c36c286ad23d9
bruno.ramaral@gmail.com <bruno.ramaral@gmail.com@cad15eb8-f57c-11dd-b556-9305e745af9e>
2009-02-19 04:04:52 +0000

    Retirando extends da classe Atendimento

--- trunk/siscab/WebContent/js/script.js
+++ trunk/siscab/WebContent/js/script.js
@@ -2,14 +2,10 @@ function mmLoadMenus() {
   if (window.mm_menu_0217221104_0) return;
   window.mm_menu_0217221104_0 = new Menu("root",185,19,"Trebuchet MS",13,"#FFFFFF","#000000","#5557A7","#BABBDC","left","middle",5,1,1000,-5,7,true,false,true,0,true,true);
   mm_menu_0217221104_0.addMenuItem("Procurar&nbsp;Ocorrências");
-  mm_menu_0217221104_0.addMenuItem("Localizar&nbsp;Ocorrências");
   mm_menu_0217221104_0.addMenuItem("Registrar&nbsp;Chamado","location='RegistrarChamado'");
   mm_menu_0217221104_0.addMenuItem("Transferir&nbsp;Atendimento");
   mm_menu_0217221104_0.addMenuItem("Verificar&nbsp;Rota");
   mm_menu_0217221104_0.addMenuItem("Acompanhar&nbsp;Atendimento");
-  mm_menu_0217221104_0.addMenuItem("Acompanhar&nbsp;Viatura");
-  mm_menu_0217221104_0.addMenuItem("Registrar&nbsp;Vítimas");
-  mm_menu_0217221104_0.addMenuItem("Registrar&nbsp;Serviços");
   mm_menu_0217221104_0.hideOnMouseOut=true;
   mm_menu_0217221104_0.bgColor='#555555';
   mm_menu_0217221104_0.menuBorder=1;
