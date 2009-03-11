91b801141d488a38ac752c71989cb5b966598a73
bruno.ramaral@gmail.com <bruno.ramaral@gmail.com@cad15eb8-f57c-11dd-b556-9305e745af9e>
2009-03-11 02:07:36 +0000

    inclusão de layout

--- siscab/WebContent/js/script.js
+++ siscab/WebContent/js/script.js
@@ -3,18 +3,17 @@ function mmLoadMenus() {
   window.mm_menu_0217221104_0 = new Menu("root",185,19,"Trebuchet MS",13,"#FFFFFF","#000000","#5557A7","#BABBDC","left","middle",5,1,1000,-5,7,true,false,true,0,true,true);
   mm_menu_0217221104_0.addMenuItem("Registrar&nbsp;Chamado","location='RegistrarChamado'");
   mm_menu_0217221104_0.addMenuItem("Transferir&nbsp;Atendimento");
-  mm_menu_0217221104_0.addMenuItem("Verificar&nbsp;Rota");
   mm_menu_0217221104_0.addMenuItem("Acompanhar&nbsp;Atendimento","location='AcompanharAtendimentos'");
-  mm_menu_0217221104_0.addMenuItem("Viaturas","location='ViaturasControle'");
+  mm_menu_0217221104_0.addMenuItem("Cadastro&nbsp;de&nbsp;Viaturas","location='ViaturasControle'");
   mm_menu_0217221104_0.hideOnMouseOut=true;
   mm_menu_0217221104_0.bgColor='#555555';
   mm_menu_0217221104_0.menuBorder=1;
   mm_menu_0217221104_0.menuLiteBgColor='#FFFFFF';
   mm_menu_0217221104_0.menuBorderBgColor='#777777';
 
-  window.mm_menu_0217221434_0 = new Menu("root",79,23,"Trebuchet MS",13,"#FFFFFF","#000000","#5557A7","#BABBDC","left","middle",5,1,1000,-5,7,true,false,true,0,true,true);
-  mm_menu_0217221434_0.addMenuItem("Usuários","location='administracao_usuario.jsp'");
-  mm_menu_0217221434_0.addMenuItem("OBMs","location='ObmControle'");
+  window.mm_menu_0217221434_0 = new Menu("root",155,23,"Trebuchet MS",13,"#FFFFFF","#000000","#5557A7","#BABBDC","left","middle",5,1,1000,-5,7,true,false,true,0,true,true);
+  mm_menu_0217221434_0.addMenuItem("Cadastro&nbsp;de&nbsp;Usuários","location='administracao_usuario.jsp'");
+  mm_menu_0217221434_0.addMenuItem("Cadastro&nbsp;de&nbsp;OBMs","location='ObmControle'");
   mm_menu_0217221434_0.hideOnMouseOut=true;
   mm_menu_0217221434_0.bgColor='#555555';
   mm_menu_0217221434_0.menuBorder=1;
@@ -22,7 +21,7 @@ function mmLoadMenus() {
   mm_menu_0217221434_0.menuBorderBgColor='#777777';
 
   window.mm_menu_0217221648_0 = new Menu("root",165,19,"Trebuchet MS",13,"#FFFFFF","#000000","#5557A7","#BABBDC","left","middle",5,1,1000,-5,7,true,false,true,0,true,true);
-  mm_menu_0217221648_0.addMenuItem("Situação&nbsp;Atendimentos");
+  mm_menu_0217221648_0.addMenuItem("Situação&nbsp;Atendimentos","location='javascript:window.close()'");
   mm_menu_0217221648_0.addMenuItem("Total&nbsp;de&nbsp;Chamados");
   mm_menu_0217221648_0.addMenuItem("Total&nbsp;de&nbsp;Atendimentos");
   mm_menu_0217221648_0.hideOnMouseOut=true;
